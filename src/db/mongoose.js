2580690908854f3bb7cbd7872e52ea7144713427
Prasad Dilip Patewar <prasadpatewar39@gmail.com>
2021-12-23 17:43:55 +0530

    Updated packages to latest version

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -7,12 +7,10 @@ const databaseName = process.env.MONGODB_DATABASE_NAME;
 const connectionURL = `${baseConnectionURL}/${databaseName}`;
 
 const connectToDB = async () => {
-	await mongoose.connect(connectionURL, {
-		useNewUrlParser: true,
-		useCreateIndex: true,
-		useUnifiedTopology: true,
-		useFindAndModify: false,
-	});
+  await mongoose.connect(connectionURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
 };
 
 module.exports = { connectToDB };
--- src/index.js
+++ src/index.js
@@ -2,38 +2,48 @@ const express = require("express");
 const { connectToDB } = require("./db/mongoose");
 const userRouter = require("./Routers/userRouter");
 const taskRouter = require("./Routers/taskRouter");
+const cors = require("cors");
 
 const startApp = async () => {
-	try {
-		//Connect to database
-		await connectToDB();
+  try {
+    //Connect to database
+    await connectToDB();
 
-		//Create configure express app
-		const port = process.env.PORT;
-		const app = express();
+    //Create configure express app
+    const port = process.env.PORT;
+    const app = express();
 
-		// Add express middlewares
-		// //Site maintainance middleware
-		// app.use((req, res, next) => {
-		// 	res.status(503).send(
-		// 		"Site is under maintainance, Please check back soon :)"
-		// 	);
-		// });
+    // Add express middlewares
+    // //Site maintainance middleware
+    // app.use((req, res, next) => {
+    // 	res.status(503).send(
+    // 		"Site is under maintainance, Please check back soon :)"
+    // 	);
+    // });
 
-		//Add json body to response object
-		app.use(express.json());
+    //Add json body to response object
+    app.use(express.json());
 
-		//Attach routers to express server
-		app.use(userRouter);
-		app.use(taskRouter);
+    // Using cors
+    app.use(
+      cors({
+        origin: "*",
+        credentials: true, //access-control-allow-credentials:true
+        optionSuccessStatus: 200,
+      })
+    );
 
-		//Start the server on port
-		app.listen(port, () => {
-			console.log("Server is up and running on port " + port);
-		});
-	} catch (error) {
-		console.log(error);
-	}
+    //Attach routers to express server
+    app.use(userRouter);
+    app.use(taskRouter);
+
+    //Start the server on port
+    app.listen(port, () => {
+      console.log("Server is up and running on port " + port);
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 startApp();
