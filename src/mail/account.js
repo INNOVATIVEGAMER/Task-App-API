8018e0de602fe781a1d66ec83a718fc2bce68e6a
Prasad Dilip Patewar <prasadpatewar39@gmail.com>
2023-02-12 18:16:58 +0530

    Formatted using prettier

--- src/mail/account.js
+++ src/mail/account.js
@@ -3,31 +3,31 @@ const sgMail = require("@sendgrid/mail");
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const sendWelcomeMail = (name, email) => {
-	const welcomeMail = {
-		to: email,
-		from: "prasadpatewar39@gmail.com",
-		subject: "Welcome to our app",
-		text: `Thank you for joining our app ${name}`,
-	};
+  const welcomeMail = {
+    to: email,
+    from: "prasadpatewar39@gmail.com",
+    subject: "Welcome to our app",
+    text: `Thank you for joining our app ${name}`,
+  };
 
-	sgMail
-		.send(welcomeMail)
-		.then(() => console.log("Welcome Email Sent"))
-		.catch((err) => console.log(err));
+  sgMail
+    .send(welcomeMail)
+    .then(() => console.log("Welcome Email Sent"))
+    .catch((err) => console.log(err));
 };
 
 const sendAccountDeleteMail = (name, email) => {
-	const accountDeleteMail = {
-		to: email,
-		from: "prasadpatewar39@gmail.com",
-		subject: "Account deleted",
-		text: `Sorry for your inconvience. ${name} hope you liked our app.`,
-	};
+  const accountDeleteMail = {
+    to: email,
+    from: "prasadpatewar39@gmail.com",
+    subject: "Account deleted",
+    text: `Sorry for your inconvience. ${name} hope you liked our app.`,
+  };
 
-	sgMail
-		.send(accountDeleteMail)
-		.then(() => console.log("Account delete Email Sent"))
-		.catch((err) => console.log(err));
+  sgMail
+    .send(accountDeleteMail)
+    .then(() => console.log("Account delete Email Sent"))
+    .catch((err) => console.log(err));
 };
 
 module.exports = { sendWelcomeMail, sendAccountDeleteMail };
