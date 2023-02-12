3a5c53c8e58007bd3640bf9238b87511dfd58de2
Prasad Dilip Patewar <prasadpatewar39@gmail.com>
2023-02-12 18:17:41 +0530

    Develop to main (Added prettier and formatted codebase) (#3)
    * Added prettier
    
    * Formatted using prettier

--- src/Routers/taskRouter.js
+++ src/Routers/taskRouter.js
@@ -7,118 +7,116 @@ const router = express.Router();
 
 //Create task in DB
 router.post("/tasks", auth, async (req, res) => {
-	try {
-		const task = new Task({ ...req.body, owner: req.user._id });
-		const taskDoc = await task.save();
-		res.status(201).send(taskDoc);
-	} catch (error) {
-		res.status(400).send(error);
-	}
+  try {
+    const task = new Task({ ...req.body, owner: req.user._id });
+    const taskDoc = await task.save();
+    res.status(201).send(taskDoc);
+  } catch (error) {
+    res.status(400).send(error);
+  }
 });
 
 //Get all tasks for a authenticated user from DB
 router.get("/tasks", auth, async (req, res) => {
-	const match = {};
-	const options = {};
-	const sort = {};
-	const sortSeparator = "_";
+  const match = {};
+  const options = {};
+  const sort = {};
+  const sortSeparator = "_";
 
-	//Filtering requested data
-	if (req.query.completed) match.completed = req.query.completed === "true";
+  //Filtering requested data
+  if (req.query.completed) match.completed = req.query.completed === "true";
 
-	//Paginating requested data
-	options.limit = parseInt(req.query.limit);
-	options.skip = parseInt(req.query.skip);
+  //Paginating requested data
+  options.limit = parseInt(req.query.limit);
+  options.skip = parseInt(req.query.skip);
 
-	//Sorting requested data
-	if (req.query.sortBy) {
-		const sortTerms = req.query.sortBy.split(sortSeparator);
-		sort[sortTerms[0]] = sortTerms[1] === "desc" ? -1 : 1;
-		options.sort = sort;
-	}
+  //Sorting requested data
+  if (req.query.sortBy) {
+    const sortTerms = req.query.sortBy.split(sortSeparator);
+    sort[sortTerms[0]] = sortTerms[1] === "desc" ? -1 : 1;
+    options.sort = sort;
+  }
 
-	try {
-		await req.user
-			.populate({ path: "tasks", match, options })
-			.execPopulate();
-		const tasks = req.user.tasks;
-		if (!tasks) {
-			res.status(404).send({ error: "Tasks not found!!!" });
-			return;
-		}
-		res.send(tasks);
-	} catch (error) {
-		res.status(500).send(error);
-	}
+  try {
+    await req.user.populate({ path: "tasks", match, options }).execPopulate();
+    const tasks = req.user.tasks;
+    if (!tasks) {
+      res.status(404).send({ error: "Tasks not found!!!" });
+      return;
+    }
+    res.send(tasks);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 });
 
 //Get task by ID for a authenticated user from DB
 router.get("/tasks/:id", auth, async (req, res) => {
-	try {
-		const _id = req.params.id;
-		const task = await Task.findOne({ _id, owner: req.user._id });
-		if (!task) {
-			res.status(404).send({ error: "Task not found!!!" });
-			return;
-		}
-		res.send(task);
-	} catch (error) {
-		res.status(500).send(error);
-	}
+  try {
+    const _id = req.params.id;
+    const task = await Task.findOne({ _id, owner: req.user._id });
+    if (!task) {
+      res.status(404).send({ error: "Task not found!!!" });
+      return;
+    }
+    res.send(task);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 });
 
 //Update task by ID for a authenticated user in DB
 router.patch("/tasks/:id", auth, async (req, res) => {
-	const updates = Object.keys(req.body);
-	const allowedUpdates = ["description", "completed"];
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ["description", "completed"];
 
-	const isValidUpdates = updates.every((update) =>
-		allowedUpdates.includes(update)
-	);
+  const isValidUpdates = updates.every((update) =>
+    allowedUpdates.includes(update)
+  );
 
-	if (!isValidUpdates) {
-		res.status(400).send({ error: "invalid updates" });
-		return;
-	}
+  if (!isValidUpdates) {
+    res.status(400).send({ error: "invalid updates" });
+    return;
+  }
 
-	try {
-		const _id = req.params.id;
-		const task = await Task.findOne({ _id, owner: req.user._id });
+  try {
+    const _id = req.params.id;
+    const task = await Task.findOne({ _id, owner: req.user._id });
 
-		if (!task) {
-			res.status(404).send({ error: "Task not found!!!" });
-			return;
-		}
+    if (!task) {
+      res.status(404).send({ error: "Task not found!!!" });
+      return;
+    }
 
-		updates.forEach((update) => {
-			task[update] = req.body[update];
-		});
+    updates.forEach((update) => {
+      task[update] = req.body[update];
+    });
 
-		const updatedTask = await task.save();
+    const updatedTask = await task.save();
 
-		res.send(updatedTask);
-	} catch (error) {
-		res.status(400).send(error);
-	}
+    res.send(updatedTask);
+  } catch (error) {
+    res.status(400).send(error);
+  }
 });
 
 //Delete task by ID for a authenticated user in DB
 router.delete("/tasks/:id", auth, async (req, res) => {
-	try {
-		const _id = req.params.id;
-		const deletedTask = await Task.findOneAndDelete({
-			_id,
-			owner: req.user._id,
-		});
-		if (!deletedTask) {
-			res.status(404).send({ error: "Task not found!!!" });
-			return;
-		}
+  try {
+    const _id = req.params.id;
+    const deletedTask = await Task.findOneAndDelete({
+      _id,
+      owner: req.user._id,
+    });
+    if (!deletedTask) {
+      res.status(404).send({ error: "Task not found!!!" });
+      return;
+    }
 
-		res.send(deletedTask);
-	} catch (error) {
-		res.status(500).send(error);
-	}
+    res.send(deletedTask);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 });
 
 module.exports = router;
