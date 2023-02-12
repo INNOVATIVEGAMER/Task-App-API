f27dc71f2fcbfa14fde23e02cc21736983080890
Prasad Dilip Patewar <prasadpatewar39@gmail.com>
2023-02-12 18:12:11 +0530

    Added vercel configuration (#2)

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -9,8 +9,8 @@ const databaseName = process.env.MONGODB_DATABASE_NAME;
 
 const connectionURL = `${baseConnectionURL}/${databaseName}`;
 
-const connectToDB = async () => {
-  await mongoose.connect(connectionURL, {
+const connectToDB = () => {
+  mongoose.connect(connectionURL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     serverApi: ServerApiVersion.v1,
--- src/index.js
+++ src/index.js
@@ -4,46 +4,40 @@ const userRouter = require("./Routers/userRouter");
 const taskRouter = require("./Routers/taskRouter");
 const cors = require("cors");
 
-const startApp = async () => {
-  try {
-    //Connect to database
-    await connectToDB();
+//Connect to database
+connectToDB();
 
-    //Create configure express app
-    const port = process.env.PORT;
-    const app = express();
+//Create configure express app
+const port = process.env.PORT;
+const app = express();
 
-    // Add express middlewares
-    // //Site maintainance middleware
-    // app.use((req, res, next) => {
-    // 	res.status(503).send(
-    // 		"Site is under maintainance, Please check back soon :)"
-    // 	);
-    // });
+// Add express middlewares
+// //Site maintainance middleware
+// app.use((req, res, next) => {
+// 	res.status(503).send(
+// 		"Site is under maintainance, Please check back soon :)"
+// 	);
+// });
 
-    //Add json body to response object
-    app.use(express.json());
+//Add json body to response object
+app.use(express.json());
 
-    // Using cors
-    app.use(
-      cors({
-        origin: "*",
-        credentials: true, //access-control-allow-credentials:true
-        optionSuccessStatus: 200,
-      })
-    );
+// Using cors
+app.use(
+  cors({
+    origin: "*",
+    credentials: true, //access-control-allow-credentials:true
+    optionSuccessStatus: 200,
+  })
+);
 
-    //Attach routers to express server
-    app.use(userRouter);
-    app.use(taskRouter);
+//Attach routers to express server
+app.use(userRouter);
+app.use(taskRouter);
 
-    //Start the server on port
-    app.listen(port, () => {
-      console.log("Server is up and running on port " + port);
-    });
-  } catch (error) {
-    console.log(error);
-  }
-};
+//Start the server on port
+app.listen(port, () => {
+  console.log("Server is up and running on port " + port);
+});
 
-startApp();
+module.exports = app;
